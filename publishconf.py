d5f650cc6932e585a848cdd9aaa257342c90a983
Donne Martin <donne.martin@gmail.com>
2016-01-27 06:20:24 -0500

    Switch to GA universal id.

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = 'UA-54747412-1'
+GOOGLE_ANALYTICS_UNIVERSAL = 'UA-54747412-1'
