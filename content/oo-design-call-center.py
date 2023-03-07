9743a31bb01cebf51ab5c2fa6b50410c28bc86d3
Donne Martin <donne.martin@gmail.com>
2023-03-07 05:11:19 -0500

    Add call center CallCenter dispatch_call

--- content/oo-design-call-center.py
+++ content/oo-design-call-center.py
@@ -89,3 +89,16 @@ class CallCenter(object):
         self.supervisors = supervisors
         self.directors = directors
         self.queued_calls = deque()
+
+    def dispatch_call(self, call):
+        if call.rank not in (Rank.OPERATOR, Rank.SUPERVISOR, Rank.DIRECTOR):
+            raise ValueError('Invalid call rank: {}'.format(call.rank))
+        employee = None
+        if call.rank == Rank.OPERATOR:
+            employee = self._dispatch_call(call, self.operators)
+        if call.rank == Rank.SUPERVISOR or employee is None:
+            employee = self._dispatch_call(call, self.supervisors)
+        if call.rank == Rank.DIRECTOR or employee is None:
+            employee = self._dispatch_call(call, self.directors)
+        if employee is None:
+            self.queued_calls.append(call)
