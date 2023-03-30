db9352925a3b866909b35da2e4b66f10991ba041
Donne Martin <donne.martin@gmail.com>
2023-03-30 05:12:07 -0400

    Add deck of cards BlackJackCard is_face_card

--- content/oo-design-deck-of-cards.py
+++ content/oo-design-deck-of-cards.py
@@ -35,3 +35,7 @@ class BlackJackCard(Card):
 
     def is_ace(self):
         return True if self._value == 1 else False
+
+    def is_face_card(self):
+        """Jack = 11, Queen = 12, King = 13"""
+        return True if 10 < self._value <= 13 else False
