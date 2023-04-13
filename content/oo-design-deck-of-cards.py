760875c8b46aa1c4aa0083f49b01be46f092d9a4
Donne Martin <donne.martin@gmail.com>
2023-04-13 05:02:03 -0400

    Add deck of cards BlackJackHand score

--- content/oo-design-deck-of-cards.py
+++ content/oo-design-deck-of-cards.py
@@ -79,3 +79,13 @@ class BlackJackHand(Hand):
 
     def __init__(self, cards):
         super(BlackJackHand, self).__init__(cards)
+
+    def score(self):
+        min_over = sys.MAXSIZE
+        max_under = -sys.MAXSIZE
+        for score in self.possible_scores():
+            if self.BLACKJACK < score < min_over:
+                min_over = score
+            elif max_under < score <= self.BLACKJACK:
+                max_under = score
+        return max_under if max_under != -sys.MAXSIZE else
