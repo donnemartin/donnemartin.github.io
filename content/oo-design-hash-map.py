6bac7eaf53d6fecf655798dbb72aaad66c55968a
Donne Martin <donne.martin@gmail.com>
2023-04-25 05:08:00 -0400

    Add hash map HashTable skeleton

--- content/oo-design-hash-map.py
+++ content/oo-design-hash-map.py
@@ -3,3 +3,9 @@ class Item(object):
     def __init__(self, key, value):
         self.key = key
         self.value = value
+
+class HashTable(object):
+
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
