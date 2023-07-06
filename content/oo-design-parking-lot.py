02b87c56454a5293625a56fd7deca4dace30f678
Donne Martin <donne.martin@gmail.com>
2023-07-05 22:07:17 -0400

    Add Parking Vehicle skeleton

--- content/oo-design-parking-lot.py
+++ content/oo-design-parking-lot.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 from enum import Enum
 
 
@@ -6,3 +7,12 @@ class VehicleSize(Enum):
     MOTORCYCLE = 0
     COMPACT = 1
     LARGE = 2
+
+
+class Vehicle(metaclass=ABCMeta):
+
+    def __init__(self, vehicle_size, license_plate, spot_size):
+        self.vehicle_size = vehicle_size
+        self.license_plate = license_plate
+        self.spot_size
+        self.spots_taken = []
