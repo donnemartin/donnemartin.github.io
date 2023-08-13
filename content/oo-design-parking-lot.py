19b5241bf902d5a3daefdaa126059c4bb94bca4f
Donne Martin <donne.martin@gmail.com>
2023-08-12 22:09:27 -0400

    Add Parking Level ParkingSpot park_vehicle

--- content/oo-design-parking-lot.py
+++ content/oo-design-parking-lot.py
@@ -117,3 +117,6 @@ class ParkingSpot(object):
         if self.vehicle is not None:
             return False
         return vehicle.can_fit_in_spot(self)
+
+    def park_vehicle(self, vehicle):
+        pass
