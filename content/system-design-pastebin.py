3130e2aea31648380842828c8b770f1d40041ece
Donne Martin <donne.martin@gmail.com>
2023-08-20 22:01:13 -0400

    Add Pastebin HitCounts extract_year_month

--- content/system-design-pastebin.py
+++ content/system-design-pastebin.py
@@ -11,3 +11,7 @@ class HitCounts(MRJob):
     def extract_url(self, line):
         """Extract the generated url from the log line."""
         pass
+
+    def extract_year_month(self, line):
+        """Return the year and month portions of the timestamp."""
+        pass
