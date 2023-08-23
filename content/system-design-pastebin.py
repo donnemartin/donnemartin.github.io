247b50dacee903ee912874491d3b25c0adcf8c66
Donne Martin <donne.martin@gmail.com>
2023-08-22 22:03:45 -0400

    Add Pastebin HitCounts mapper

--- content/system-design-pastebin.py
+++ content/system-design-pastebin.py
@@ -15,3 +15,16 @@ class HitCounts(MRJob):
     def extract_year_month(self, line):
         """Return the year and month portions of the timestamp."""
         pass
+
+    def mapper(self, _, line):
+        """Parse each log line, extract and transform relevant lines.
+
+        Emit key value pairs of the form:
+
+        (2016-01, url0), 1
+        (2016-01, url0), 1
+        (2016-01, url1), 1
+        """
+        url = self.extract_url(line)
+        period = self.extract_year_month(line)
+        yield (period, url), 1
