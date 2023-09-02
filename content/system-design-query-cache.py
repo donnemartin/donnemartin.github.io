3c725796d6a74b3a076b5794e7a2de60ce4d4db7
Donne Martin <donne.martin@gmail.com>
2023-09-01 22:09:12 -0400

    Add QueryCache QueryApi parse_query

--- content/system-design-query-cache.py
+++ content/system-design-query-cache.py
@@ -6,3 +6,9 @@ class QueryApi(object):
     def __init__(self, memory_cache, reverse_index_cluster):
         self.memory_cache = memory_cache
         self.reverse_index_cluster = reverse_index_cluster
+
+    def parse_query(self, query):
+        """Remove markup, break text into terms, deal with typos,
+        normalize capitalization, convert to use boolean operations.
+        """
+        ...
