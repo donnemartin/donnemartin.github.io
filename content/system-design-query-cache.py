28160aa5ad5e71149db6d985923d94cb36ac8b8b
Donne Martin <donne.martin@gmail.com>
2023-09-03 22:07:05 -0400

    Add QueryCache QueryApi process_query

--- content/system-design-query-cache.py
+++ content/system-design-query-cache.py
@@ -12,3 +12,11 @@ class QueryApi(object):
         normalize capitalization, convert to use boolean operations.
         """
         ...
+
+    def process_query(self, query):
+        query = self.parse_query(query)
+        results = self.memory_cache.get(query)
+        if results is None:
+            results = self.reverse_index_cluster.process_search(query)
+            self.memory_cache.set(query, results)
+        return results
