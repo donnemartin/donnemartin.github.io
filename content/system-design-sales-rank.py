bcfd0a6110c2ca6eb81c4f7a3fc981d3dcdcec53
Donne Martin <donne.martin@gmail.com>
2023-09-25 22:05:34 -0400

    Add SalesRank SalesRanker within_past_week

--- content/system-design-sales-rank.py
+++ content/system-design-sales-rank.py
@@ -7,3 +7,7 @@ class SalesRanker(MRJob):
 
     def __init__(self):
         pass
+
+    def within_past_week(self, timestamp):
+        """Return True if timestamp is within past week, False otherwise."""
+        ...
