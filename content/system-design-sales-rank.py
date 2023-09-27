798e733bf5fe8441144b40248823ac713a8795f0
Donne Martin <donne.martin@gmail.com>
2023-09-26 22:02:57 -0400

    Add SalesRank SalesRanker mapper

--- content/system-design-sales-rank.py
+++ content/system-design-sales-rank.py
@@ -11,3 +11,19 @@ class SalesRanker(MRJob):
     def within_past_week(self, timestamp):
         """Return True if timestamp is within past week, False otherwise."""
         ...
+
+    def mapper(self, _, line):
+        """Parse each log line, extract and transform relevant lines.
+
+        Emit key value pairs of the form:
+
+        (foo, p1), 2
+        (bar, p1), 2
+        (bar, p1), 1
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
+        """
+        timestamp, product_id, category, quantity = line.split('\t')
+        if self.within_past_week(timestamp):
+            yield (category, product_id), quantity
