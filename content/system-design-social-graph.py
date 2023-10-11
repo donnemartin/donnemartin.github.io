657324f707e627bd3c944837fc0cefdf1e45c509
Donne Martin <donne.martin@gmail.com>
2023-10-10 22:02:36 -0400

    Add Social Graph bfs

--- content/system-design-social-graph.py
+++ content/system-design-social-graph.py
@@ -12,3 +12,20 @@ class Graph(object):
 
     def __init__(self):
         pass
+
+    def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
