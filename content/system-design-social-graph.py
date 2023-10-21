19a0fc13cece383b75fb8ad2d74ba9ba8b6cfbb7
Donne Martin <donne.martin@gmail.com>
2023-10-20 22:03:00 -0400

    Add Social Graph PersonServer get_people

--- content/system-design-social-graph.py
+++ content/system-design-social-graph.py
@@ -53,3 +53,10 @@ class PersonServer(object):
 
     def __init__(self):
         self.people = {}  # key: person_id, value: person
+
+    def get_people(self, ids):
+        results = []
+        for id in ids:
+            if id in self.people:
+                results.append(self.people[id])
+        return results
