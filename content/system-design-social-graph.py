2c23c47b4c50cb24a2460221ecd99525ff02f946
Donne Martin <donne.martin@gmail.com>
2023-10-22 22:08:45 -0400

    Add Social Graph UserGraphService skeleton

--- content/system-design-social-graph.py
+++ content/system-design-social-graph.py
@@ -60,3 +60,11 @@ class PersonServer(object):
             if id in self.people:
                 results.append(self.people[id])
         return results
+
+
+class UserGraphService(object):
+
+    def __init__(self, person_ids, lookup):
+        self.lookup = lookup
+        self.person_ids = person_ids
+        self.visited_ids = set()
