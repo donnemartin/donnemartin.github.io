eb4f862c5c3299f64519ec9d52ec83fe7943a2fd
Donne Martin <donne.martin@gmail.com>
2023-10-28 22:08:56 -0400

    Add Web Crawler PagesDataStore add_link_to_crawl

--- content/system-design-web-crawler.py
+++ content/system-design-web-crawler.py
@@ -6,3 +6,7 @@ class PagesDataStore(object):
     def __init__(self, db):
         self.db = db
         pass
+
+    def add_link_to_crawl(self, url):
+        """Add the given link to `links_to_crawl`."""
+        pass
